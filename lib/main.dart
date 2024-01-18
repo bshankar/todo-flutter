745ade6ee3add2c1a67a6527fb3f5047fd303091
bshankar <ebhavanishankar@gmail.com>
2024-01-18 21:05:25 +0530

    Build basic riverpod provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/widgets/home.dart';
 
 void main() {
-  runApp(const TodoApp());
+  runApp(const ProviderScope(child: TodoApp()));
 }
 
 class TodoApp extends StatelessWidget {
