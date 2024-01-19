53bfcf8d800a954bed67b63d0158af7ab551b729
bshankar <ebhavanishankar@gmail.com>
2024-01-19 10:59:29 +0530

    Refactor: separate models

---
+++ lib/models/todo.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'todo.freezed.dart';
+
+@freezed
+class Todo with _$Todo {
+  factory Todo(
+      {required String id,
+      required String title,
+      required bool isDone}) = _Todo;
+}
