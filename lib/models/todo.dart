f82a2529b65e8c856b0a781f9c7c9bf0bed52618
bshankar <ebhavanishankar@gmail.com>
2024-01-20 10:39:45 +0530

    Add description and priority for todos

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -3,15 +3,33 @@ import 'package:uuid/uuid.dart';
 
 part 'todo.freezed.dart';
 
+enum TodoPriority {
+  high,
+  medium,
+  low,
+}
+
 @freezed
 class Todo with _$Todo {
-  factory Todo({required String title}) {
-    return Todo._withId(id: const Uuid().v4(), title: title, isDone: false);
+  factory Todo({
+    required String title,
+    required String description,
+    TodoPriority? priority,
+  }) {
+    return Todo._withId(
+      id: const Uuid().v4(),
+      title: title,
+      description: description,
+      completed: false,
+      priority: priority ?? TodoPriority.low,
+    );
   }
 
   factory Todo._withId({
     required String id,
     required String title,
-    required bool isDone,
+    required String description,
+    required bool completed,
+    required TodoPriority priority,
   }) = _Todo;
 }
--- lib/providers/todos.dart
+++ lib/providers/todos.dart
@@ -8,16 +8,34 @@ class TodosNotifier extends _$TodosNotifier {
   @override
   List<Todo> build() {
     return [
-      Todo(title: 'Wash clothes'),
-      Todo(title: 'Pay rent'),
-      Todo(title: 'Cut my nails'),
-      Todo(title: 'Shave my head'),
-      Todo(title: 'Buy some candles'),
+      Todo(
+        title: 'Wash clothes',
+        description: 'Clothes are very smelly. I need to wash them asap.',
+      ),
+      Todo(
+        title: 'Pay rent',
+        description: 'My landlord is about to get angry.',
+      ),
+      Todo(
+        title: 'Cut my nails',
+        description: 'Fingers are looking quite bad',
+      ),
+      Todo(
+        title: 'Shave my head',
+        description: 'Save money on shampoo',
+      ),
+      Todo(
+        title: 'Buy some candles',
+        description: 'Powercuts are coming',
+      ),
     ];
   }
 
-  void addTodo(String title) {
-    state = [Todo(title: title), ...state];
+  void addTodo(String title, String description, TodoPriority? priority) {
+    state = [
+      Todo(title: title, description: description, priority: priority),
+      ...state,
+    ];
   }
 
   void deleteTodo(String id) {
@@ -30,14 +48,14 @@ class TodosNotifier extends _$TodosNotifier {
   void toggleDone(String id) {
     state = [
       for (final todo in state)
-        if (todo.id == id) todo.copyWith(isDone: !todo.isDone) else todo,
+        if (todo.id == id) todo.copyWith(completed: !todo.completed) else todo,
     ];
   }
 
   void updateTitle({required String id, required String newTitle}) {
     state = [
       for (final todo in state)
-        if (todo.id == id) todo.copyWith(isDone: !todo.isDone) else todo,
+        if (todo.id == id) todo.copyWith(completed: !todo.completed) else todo,
     ];
   }
 }
