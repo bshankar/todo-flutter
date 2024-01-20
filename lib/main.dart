c484bc79eae2208494685fca4d4bf4114177db7b
bshankar <ebhavanishankar@gmail.com>
2024-01-20 10:40:03 +0530

    Add routing and bootstrap database

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:go_router/go_router.dart';
 import 'package:todo_app/widgets/home.dart';
 
 part 'main.g.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const Home(),
+    ),
+  ],
+);
 
 void main() {
   runApp(const ProviderScope(child: TodoApp()));
@@ -11,12 +21,11 @@ void main() {
 
 @swidget
 Widget _todoApp(BuildContext context) {
-  return MaterialApp(
-    title: 'Todos',
+  return MaterialApp.router(
+    routerConfig: _router,
     theme: ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
     ),
-    home: const Home(),
   );
 }
