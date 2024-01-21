7780fb9a59324c779eb3bdf1755343a31631cc21
bshankar <ebhavanishankar@gmail.com>
2024-01-21 09:55:20 +0530

    Implement back button functionality

--- lib/screens/todo_edit.dart
+++ lib/screens/todo_edit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
+import 'package:go_router/go_router.dart';
 import 'package:todo_app/widgets/todo_edit.dart';
 
 part 'todo_edit.g.dart';
@@ -9,7 +10,11 @@ Widget _todoEditScreen(BuildContext context, {String? id}) {
   return Scaffold(
     appBar: AppBar(
       title: const Text('Edit Todo'),
-      leading: const BackButton(),
+      leading: BackButton(
+        onPressed: () {
+          GoRouter.of(context).push('/');
+        },
+      ),
     ),
     body: TodoEdit(id),
   );
